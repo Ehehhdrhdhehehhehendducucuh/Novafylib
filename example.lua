8a58ad909dcb8062f01129847ed4c37d76ebcd2f
Ehehhdrhdhehehhehendducucuh <142185912+Ehehhdrhdhehehhehendducucuh@users.noreply.github.com>
2024-12-28 14:14:19 +0800

    Create example.lua

---
+++ example.lua
@@ -0,0 +1,3 @@
+local NotificationLib = loadstring(game:HttpGet("https://pastefy.app/qNbQnyMO/raw"))() -- lib
+
+NotificationLib:CreateNotification("What it will say?", 5, Color3.fromRGB(255, 255, 255), Color3.fromRGB(50, 50, 50), "rbxassetid://16618974604") -- actual thing is notifying
