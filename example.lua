87be62d6bf6e630cd3140bb3e433142d1f5fbb5b
Ehehhdrhdhehehhehendducucuh <142185912+Ehehhdrhdhehehhehendducucuh@users.noreply.github.com>
2024-12-28 17:02:27 +0800

    Update example.lua

--- example.lua
+++ example.lua
@@ -2,6 +2,6 @@
     Remember dont skid.
 --]]
 
-local NovafyLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ehehhdrhdhehehhehendducucuh/Novafylib/refs/heads/main/obf_zJhj10I4140tKWx32izk5Dv4RG3JdM3K6XoCHe6hwkPo370vIelUwZlQ2Y7bJ3MI.lua.txt4Zr95iH.lua.txt"))() -- lib
+local NovafyLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ehehhdrhdhehehhehendducucuh/Novafylib/refs/heads/main/obf_zJhj10I4140tKWx32izk5Dv4RG3JdM3K6XoCHe6hwkPo370vIelUwZlQ2Y7bJ3MI.lua.txt"))() -- lib
 
-NovafyLib:CreateNotification("What it will say?", 5, Color3.fromRGB(255, 255, 255), Color3.fromRGB(50, 50, 50), "rbxassetid://16618974604") -- actual thing is notifying
+NovafyLib:CreateNotification("What it will say?", 5, Color3.fromRGB(255, 255, 255), Color3.fromRGB(50, 50, 50), "rbxassetid://16618974557") -- actual thing is notifying
