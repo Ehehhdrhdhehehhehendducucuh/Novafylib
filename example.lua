b3bfb955a51e6c5723b59994e49a1152127b2c05
Ehehhdrhdhehehhehendducucuh <142185912+Ehehhdrhdhehehhehendducucuh@users.noreply.github.com>
2024-12-28 14:27:35 +0800

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,3 +1,7 @@
-local NotificationLib = loadstring(game:HttpGet("https://pastefy.app/qNbQnyMO/raw"))() -- lib
+--[[
+    dont skid it
+--]]
+
+local NotificationLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ehehhdrhdhehehhehendducucuh/Novafylib/refs/heads/main/Novafy.txt"))() -- lib
 
 NotificationLib:CreateNotification("What it will say?", 5, Color3.fromRGB(255, 255, 255), Color3.fromRGB(50, 50, 50), "rbxassetid://16618974604") -- actual thing is notifying
